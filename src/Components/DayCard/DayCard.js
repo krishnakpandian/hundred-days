ae971731b90116af7c850e658729855f8aa82875
kpandian <kpandian@ucsc.edu>
2020-08-16 18:15:30 -0700

    Updating with fail cards

--- src/Components/DayCard/DayCard.js
+++ src/Components/DayCard/DayCard.js
@@ -4,22 +4,42 @@ import Card from 'react-bootstrap/Card';
 import Fade from 'react-reveal/Fade';
 
 const DayCard = (props) => {
-    return (
+    if (props.data.challenge === "Fail") {
+        return(
         <React.Fragment>
-            <div class="card-container">
+            <div class="card-container fail">
                 <Fade down delay={300} distance={"0.5em"}>
                     <Card>
                         <Card.Body>
                             <Card.Title>Day: {props.index}</Card.Title>
                             <Card.Subtitle className="mb-2 text-muted">{props.data.challenge}</Card.Subtitle>
                             <Card.Text>{props.data.description}</Card.Text>
-                            <Card.Link href={props.data.link}>{props.data.date}</Card.Link>
+                            <Card.Link>{props.data.date}</Card.Link>
                         </Card.Body>
                     </Card>
                 </Fade>
             </div>
         </React.Fragment>
-    );
+        );
+    }
+    else {
+        return (
+            <React.Fragment>
+                <div class="card-container">
+                    <Fade down delay={300} distance={"0.5em"}>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Day: {props.index}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{props.data.challenge}</Card.Subtitle>
+                                <Card.Text>{props.data.description}</Card.Text>
+                                <Card.Link href={props.data.link}>{props.data.date}</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Fade>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default DayCard;
